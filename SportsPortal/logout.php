10b822429593493df46e2eb16d254580866ada32
Dattatreya Nayak <dattu4621@gmail.com>
2017-02-16 22:21:03 +0530

    Update logout.php

--- SportsPortal/logout.php
+++ SportsPortal/logout.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
 $db=mysqli_connect("localhost","root","Dattu@3121","SportsPortal") or die('error querying database');
-$current_timestamp = time();
+//$current_timestamp = time();
 if (isset($_SESSION['user']) && isset($_SESSION['admin'])) {
+	echo "hello";
 	$user=$_SESSION['user'];
 	$admin = $_SESSION['admin'];
-	$current_timestamp_date = date("d F Y H:i:s", $current_timestamp);
-	$query3 = "UPDATE Register SET logout='$current_timestamp_date' WHERE user='$user' AND sysadmin = '$admin'";
+	$query3 = "UPDATE Register SET logout=now() WHERE user='$user' AND sysadmin = '$admin' ORDER BY login DESC LIMIT 1";
 	$result3 = mysqli_query($db,$query3) or die(mysqli_error($db));
-	
-	header('Location: http://127.0.0.1/SportsPortal/login.php');
+	header('Location: http://127.0.0.1/SportsPortal/index.php');
 	exit();
+	session_unset();
+	session_destroy();
 }
 session_unset();
-	session_destroy();
+session_destroy();
 ?>
